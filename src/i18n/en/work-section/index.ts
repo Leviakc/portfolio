fe52d7fac7d55c0093aa3d474abb0398846bf70c
Juan gomen <gomen98.jg@gmail.com>
2026-03-22 17:54:37 -0600

    feat: update project card component; add data-category attribute for improved filtering

--- src/i18n/en/work-section/index.ts
+++ src/i18n/en/work-section/index.ts
@@ -1,7 +1,7 @@
 import workProjects from "./projects";
 export default {
   "work.title": "portafolio",
-  "work.tabs": ["todos", "personal", "freelance", "tools"],
+  "work.tabs": ["all", "personal", "freelance", "tools"],
   "work.projects": workProjects,
   "work.modal.viewProject": "View project",
   "work.modal.github": "GitHub",
